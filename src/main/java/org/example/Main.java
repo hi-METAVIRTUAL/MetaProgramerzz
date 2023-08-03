b0a98eaf48db6c85c42553e54c9168fb91991c7f
wjs960 <wjs960@gmail.com>
2023-08-03 17:30:25 +0900

    2023-08-03 sj

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("열어분 레포 제목처럼 우린 메타 프로그래머즈으으가 됩시다");
+
+        System.out.println("팀원들에게 하고싶은 말 ...");
     }
 }
