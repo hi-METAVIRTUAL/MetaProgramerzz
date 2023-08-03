40dad18371ee1501503bc706bec837ed6e3ea6fa
babyybiss <younghyoun.l@gmail.com>
2023-08-03 18:08:32 +0900

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("열어분 레포 제목처럼 우린 메타 프로그래머즈으으가 됩시다");
+
+        System.out.println("hihi");
+        System.out.println("여럽지않다");
     }
 }
