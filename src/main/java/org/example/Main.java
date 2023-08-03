25dcd9c719f84a8f3a78e572e8cb3cb58c5df77d
wjs960 <wjs960@gmail.com>
2023-08-03 17:52:40 +0900

    2023-08-03 sj 브렌치 테스트2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println("열어분 레포 제목처럼 우린 메타 프로그래머즈으으가 됩시다");
 
         System.out.println("팀원들에게 하고싶은 말 ...");
-        System.out.println("다시");
+        System.out.println("다시");   
 
         
 
