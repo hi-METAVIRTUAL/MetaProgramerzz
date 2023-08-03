732c93103003898b46a5f411f04358aff5b41f99
jiyeong08 <a01086283142@gmail.com>
2023-08-03 17:39:35 +0900

    jy-1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("열어분 레포 제목처럼 우린 메타 프로그래머즈으으가 됩시다");
-        System.out.println("Yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+        System.out.println("Yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
     }
 
 }
